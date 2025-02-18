f3b00653e8c3cb55dc3d396b597cbe2d77d0820f
Martin Schobben <martin.schobben@geo.tuwien.ac.at>
2025-02-18 15:47:55 +0100

    reprojection of decision

--- src/dask_flood_mapper/calculation.py
+++ src/dask_flood_mapper/calculation.py
@@ -4,43 +4,48 @@ import numpy as np
 import xarray as xr
 from dask.distributed import wait
 
+
 def calculate_flood_dc(sig0_dc, plia_dc, hpar_dc):
     """Merge four data cubes and apply processing steps to clean and filter the dataset.
-    wcover_dc is optional. """
-    
+    wcover_dc is optional."""
 
     flood_dc = xr.merge([sig0_dc, plia_dc, hpar_dc])
-    flood_dc = (flood_dc
-                .reset_index("orbit", drop=True)
-                .rename({"orbit": "time"})
-                .dropna(dim="time", how="all", subset=["sig0"]))
+    flood_dc = (
+        flood_dc.reset_index("orbit", drop=True)
+        .rename({"orbit": "time"})
+        .dropna(dim="time", how="all", subset=["sig0"])
+    )
 
     flood_dc = flood_dc.persist()
-    wait(flood_dc) 
+    wait(flood_dc)
 
     return flood_dc
 
+
 def remove_speckles(flood_output, window_size=5):
     """Apply a rolling median filter to smooth the dataset spatially over longitude and latitude."""
-    
-    flood_output = (flood_output
-                    .rolling({"longitude": window_size, "latitude": window_size}, center=True)
-                    .median(skipna=True)
-                    .persist())
 
-    wait(flood_output) 
-    
+    flood_output = (
+        flood_output.rolling({"x": window_size, "y": window_size}, center=True)
+        .median(skipna=True)
+        .persist()
+    )
+
+    wait(flood_output)
+
     return flood_output
 
+
 def calc_water_likelihood(dc):
-    return  dc.MPLIA * -0.394181 + -4.142015
+    return dc.MPLIA * -0.394181 + -4.142015
+
 
 def harmonic_expected_backscatter(dc):
     w = np.pi * 2 / 365
-    
+
     t = dc.time.dt.dayofyear
     wt = w * t
-    
+
     M0 = dc.M0
     S1 = dc.S1
     S2 = dc.S2
@@ -49,25 +54,31 @@ def harmonic_expected_backscatter(dc):
     C2 = dc.C2
     C3 = dc.C3
     hm_c1 = (M0 + S1 * np.sin(wt)) + (C1 * np.cos(wt))
-    hm_c2 = ((hm_c1 + S2 * np.sin(2 * wt)) + C2 * np.cos(2 * wt))
-    hm_c3 = ((hm_c2 + S3 * np.sin(3 * wt)) + C3 * np.cos(3 * wt))
+    hm_c2 = (hm_c1 + S2 * np.sin(2 * wt)) + C2 * np.cos(2 * wt)
+    hm_c3 = (hm_c2 + S3 * np.sin(3 * wt)) + C3 * np.cos(3 * wt)
     return hm_c3
 
+
 def bayesian_flood_decision(dc):
-    
     nf_std = 2.754041
     sig0 = dc.sig0
     std = dc.STD
     wbsc = dc.wbsc
     hbsc = dc.hbsc
 
-    f_prob = (1.0 / (std * np.sqrt(2 * np.pi))) * np.exp(-0.5 * \
-        (((sig0 - wbsc) / nf_std) ** 2))
-    nf_prob = (1.0 / (nf_std * np.sqrt(2 * np.pi))) * np.exp(-0.5 * \
-        (((sig0 - hbsc) / nf_std) ** 2))
-    
+    f_prob = (1.0 / (std * np.sqrt(2 * np.pi))) * np.exp(
+        -0.5 * (((sig0 - wbsc) / nf_std) ** 2)
+    )
+    nf_prob = (1.0 / (nf_std * np.sqrt(2 * np.pi))) * np.exp(
+        -0.5 * (((sig0 - hbsc) / nf_std) ** 2)
+    )
+
     evidence = (nf_prob * 0.5) + (f_prob * 0.5)
     nf_post_prob = (nf_prob * 0.5) / evidence
     f_post_prob = (f_prob * 0.5) / evidence
-    decision = xr.where(np.isnan(f_post_prob) | np.isnan(nf_post_prob), np.nan, np.greater(f_post_prob, nf_post_prob))
+    decision = xr.where(
+        np.isnan(f_post_prob) | np.isnan(nf_post_prob),
+        np.nan,
+        np.greater(f_post_prob, nf_post_prob),
+    )
     return nf_post_prob, f_post_prob, decision
--- src/dask_flood_mapper/processing.py
+++ src/dask_flood_mapper/processing.py
@@ -5,78 +5,79 @@ from dask_flood_mapper.setup import config
 import numpy as np
 import xarray as xr
 from dask.distributed import wait
-
+from shapely.geometry import box
+from shapely import affinity
+from pyproj import Transformer
+from shapely.ops import transform
 
 # import parameters from config.yaml file
 crs = config["base"]["crs"]
 chunks = config["base"]["chunks"]
-resolution =  config["base"]["resolution"]
+resolution = config["base"]["resolution"]
 groupby = config["base"]["groupby"]
-bands_hpar= ("C1", "C2", "C3", "M0", "S1", "S2", "S3", "STD") # not possible to add to yaml file since is a ("a", "v") type
-bands_plia = "MPLIA" 
+bands_hpar = (
+    "C1",
+    "C2",
+    "C3",
+    "M0",
+    "S1",
+    "S2",
+    "S3",
+    "STD",
+)  # not possible to add to yaml file since is a ("a", "v") type
+bands_plia = "MPLIA"
+
 
 # pre-processing
-def prepare_sig0dc(items_sig0,bbox):
-
-    sig0_dc = odc_stac.load(items_sig0,
-                        bands= "VV",
-                        crs=crs,
-                        chunks=chunks,
-                        resolution=resolution,
-                        bbox=bbox,
-                        resampling="bilinear",
-                        groupby=groupby,
-                        )
-    return sig0_dc
-
-def prepare_dc(items_hpar, bbox, bands):
-    hpar_dc = odc_stac.load(items_hpar,
-                        bands=bands,
-                        crs=crs,
-                        chunks=chunks,
-                        resolution=resolution,
-                        bbox=bbox,
-                        groupby=groupby,
-                        )
-    
-    return hpar_dc
+def prepare_dc(items, bbox, bands):
+    return odc_stac.load(
+        items,
+        bands=bands,
+        chunks=chunks,
+        bbox=bbox,
+        groupby=groupby,
+    )
 
 # processing
 def process_sig0_dc(sig0_dc, items_sig0, bands):
-    
-    sig0_dc = (post_process_eodc_cube(sig0_dc, items_sig0, bands)
-               .rename_vars({"VV": "sig0"})
-               .assign_coords(orbit=("time", extract_orbit_names(items_sig0)))
-               .dropna(dim="time", how="all")
-               .sortby("time"))
-    
+    sig0_dc = (
+        post_process_eodc_cube(sig0_dc, items_sig0, bands)
+        .rename_vars({"VV": "sig0"})
+        .assign_coords(orbit=("time", extract_orbit_names(items_sig0)))
+        .dropna(dim="time", how="all")
+        .sortby("time")
+    )
+
     __, indices = np.unique(sig0_dc.time, return_index=True)
     indices.sort()
 
     orbit_sig0 = sig0_dc.orbit[indices].data
 
     sig0_dc = sig0_dc.groupby("time").mean(skipna=True)
-    
+
     sig0_dc = sig0_dc.assign_coords(orbit=("time", orbit_sig0))
 
     sig0_dc = sig0_dc.persist()
-    wait(sig0_dc)  
+    wait(sig0_dc)
 
     return sig0_dc, orbit_sig0
 
-def process_datacube(datacube, items_dc,orbit_sig0, bands ):
-    
-    datacube = post_process_eodc_cube(datacube, items_dc, bands).rename({"time": "orbit"})
-    
+
+def process_datacube(datacube, items_dc, orbit_sig0, bands):
+    datacube = post_process_eodc_cube(datacube, items_dc, bands).rename(
+        {"time": "orbit"}
+    )
+
     datacube["orbit"] = extract_orbit_names(items_dc)
-    
+
     datacube = datacube.groupby("orbit").mean(skipna=True)
 
     datacube = datacube.sel(orbit=orbit_sig0)
 
     datacube = datacube.persist()
-    wait(datacube)  
+    wait(datacube)
     return datacube
+
 
 # post-processing
 def post_process_eodc_cube(dc: xr.Dataset, items, bands):
@@ -86,17 +87,23 @@ def post_process_eodc_cube(dc: xr.Dataset, items, bands):
         dc[i] = post_process_eodc_cube_(dc[i], items, i)
     return dc
 
+
 def post_process_eodc_cube_(dc: xr.DataArray, items, band):
-    scale = items[0].assets[band].extra_fields.get('raster:bands')[0]['scale']
-    nodata = items[0].assets[band].extra_fields.get('raster:bands')[0]['nodata']
+    scale = items[0].assets[band].extra_fields.get("raster:bands")[0]["scale"]
+    nodata = items[0].assets[band].extra_fields.get("raster:bands")[0]["nodata"]
     # Apply the scaling and nodata masking logic
     return dc.where(dc != nodata) / scale
 
 
 def extract_orbit_names(items):
-    return np.array([items[i].properties["sat:orbit_state"][0].upper() + \
-                     str(items[i].properties["sat:relative_orbit"]) \
-                     for i in range(len(items))])
+    return np.array(
+        [
+            items[i].properties["sat:orbit_state"][0].upper()
+            + str(items[i].properties["sat:relative_orbit"])
+            for i in range(len(items))
+        ]
+    )
+
 
 def post_processing(dc):
     dc = dc * np.logical_and(dc.MPLIA >= 27, dc.MPLIA <= 48)
@@ -104,7 +111,13 @@ def post_processing(dc):
     land_bsc_lower = dc.hbsc - 3 * dc.STD
     land_bsc_upper = dc.hbsc + 3 * dc.STD
     water_bsc_upper = dc.wbsc + 3 * 2.754041
-    mask_land_outliers = np.logical_and(dc.sig0 > land_bsc_lower, dc.sig0 < land_bsc_upper)
+    mask_land_outliers = np.logical_and(
+        dc.sig0 > land_bsc_lower, dc.sig0 < land_bsc_upper
+    )
     mask_water_outliers = dc.sig0 < water_bsc_upper
     dc = dc * (mask_land_outliers | mask_water_outliers)
-    return  (dc * (dc.f_post_prob > 0.8)).decision
+    return (dc * (dc.f_post_prob > 0.8)).decision
+
+
+def reproject_equi7grid(dc, target_epsg):
+    return dc.rio.reproject("EPSG:4326")
