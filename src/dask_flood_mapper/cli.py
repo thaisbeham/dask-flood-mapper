080bc422ab8cc8b54f24488660d13d0984a793eb
Martin Schobben <martin.schobben@geo.tuwien.ac.at>
2025-04-08 14:59:14 +0200

    make CLI

--- src/dask_flood_mapper/cli.py
+++ src/dask_flood_mapper/cli.py
@@ -1,12 +1,12 @@
 import webbrowser
 import threading
-from app import app  # or however your app is imported
+from dask_flood_mapper.app import app  # or however your app is imported
 
 
 def open_browser():
     webbrowser.open_new("http://127.0.0.1:5000")
 
 
-if __name__ == "__main__":
+def main():
     threading.Timer(1.5, open_browser).start()
     app.run()
